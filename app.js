e3c042073d05ced5d4a210196fbd42fcd030877e
Shahid Shaikh <UnixRoot@Shahids-MacBook-Air.local>
2019-05-17 01:15:01 +0530

    code update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-var express           =     require('express')
+const express         =     require('express')
   , passport          =     require('passport')
-  , util              =     require('util')
   , FacebookStrategy  =     require('passport-facebook').Strategy
   , session           =     require('express-session')
   , cookieParser      =     require('cookie-parser')
@@ -10,19 +9,12 @@ var express           =     require('express')
   , app               =     express();
 
 //Define MySQL parameter in Config.js file.
-var connection = mysql.createConnection({
+const pool = mysql.createPool({
   host     : config.host,
   user     : config.username,
   password : config.password,
   database : config.database
 });
-
-//Connect to Database only if Config.js parameter is set.
-
-if(config.use_database==='true')
-{
-    connection.connect();
-}
 
 // Passport session setup.
 passport.serializeUser(function(user, done) {
@@ -44,20 +36,17 @@ passport.use(new FacebookStrategy({
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
       //Check whether the User exists or not using profile.id
-      if(config.use_database==='true')
-      {
-      connection.query("SELECT * from user_info where user_id="+profile.id,function(err,rows,fields){
-        if(err) throw err;
-        if(rows.length===0)
-          {
-            console.log("There is no such user, adding now");
-            connection.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
-          }
-          else
-            {
+      if(config.use_database) {
+        // if sets to true
+        pool.query("SELECT * from user_info where user_id="+profile.id, (err,rows) => {
+          if(err) throw err;
+          if(rows && rows.length === 0) {
+              console.log("There is no such user, adding now");
+              pool.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
+          } else {
               console.log("User already exists in database");
-            }
-          });
+          }
+        });
       }
       return done(null, profile);
     });
--- configuration/config.js
+++ configuration/config.js
@@ -1,15 +1,15 @@
-module.exports={
-/*
-* This file contains the configurations information of Twitter login app.
-* It consists of Twitter app information, database information.
-*/
+module.exports = {
+  /*
+   * This file contains the configurations information of Twitter login app.
+   * It consists of Twitter app information, database information.
+   */
 
-	"facebook_api_key" 		: 			"",
-	"facebook_api_secret"	:				"",
-	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
-	"use_database"				:				"false",
-	"host"								:				"localhost",
-	"username"						:				"root",
-	"password"						:				"",
-	"database"						:				""
-}
+  facebook_api_key: "",
+  facebook_api_secret: "",
+  callback_url: "http://localhost:3000/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
